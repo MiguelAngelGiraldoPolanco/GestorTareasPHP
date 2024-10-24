828135ad6b6c9dff81d2dee677ec80ce2a3b6461
MiguelAngel <miguelgiraldo1116@gmail.com>
2024-10-24 14:26:08 -0400

    agrege header y footer

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
     $_SESSION ['user']="Miguel";
+    include "views/header.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GestorTareas</title>
-</head>
-<body>
+    <div>
     <form action = "login.php" method="POST">
         Nombre Tarea: 
         <input type="text" name="name" placeholder="Ingrese el nombre de la tarea"><br>
@@ -26,5 +20,5 @@
 
         <input type="submit" value="Login">        
     </form>
-</body>
-</html>
+    </div>
+<?php include "views/footer.php"; ?>
---
+++ views/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>&copy; Gestor De Tareas PHP</p>
+    </footer>
+</body>
+</html>
